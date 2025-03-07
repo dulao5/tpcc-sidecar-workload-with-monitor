1024b0d56d040a26e88e6f93c7d9f467a2ee3379
dulao5 <dulao5@gmail.com>
2025-03-07 11:25:50 +0000

    high-qps workload

--- src/public/index.php
+++ src/public/index.php
@@ -8,6 +8,23 @@ $dbUser = getenv("DB_USER") ?: "root";
 $dbPass = getenv("DB_PASS") ?: "";
 $dbName = getenv("DB_NAME") ?: "test";
 
+$beginTime = microtime(true);
+$doneTasks = ["start_process ".date("Y-m-d H:i:s")];
+
+function traceTasks($taskname) {
+	global $beginTime;
+	global $doneTasks;
+	$doneTasks[] = $taskname." (time: ".(microtime(true)-$beginTime)."s)";
+}
+function getDebugInfo($e = null) {
+	global $doneTasks;
+	$res = "doneTasks:\t". join("\t", $doneTasks);
+	if (!is_null($e)) {
+		$res .= "\t error:\t".str_replace("\n", "\t", $e->getTraceAsString());
+	}
+	return $res;
+}
+
 try {
 
     // connect to mysql database (port 4000) using PDO
@@ -15,6 +32,7 @@ try {
         PDO::ATTR_TIMEOUT => 5,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     ));
+    traceTasks("do_connect");
 
     $maxWarehouseId = 2;
     $maxDistrictId = 10;
@@ -24,13 +42,16 @@ try {
     $randomCustomerId = rand(1, $maxCustomerId);
 
     $stmt = $db->query("begin");
+    traceTasks("do_begin");
 
     // sleep 500ms to simulate a slow connection
     usleep(60000);
+    traceTasks("do_sleep");
 
     // create a query to get all the data from the table
     $stmt = $db->query("SELECT * FROM customer where c_w_id = $maxWarehouseId and  c_d_id = $randomDistrictId and c_id = $randomCustomerId");
     $datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    traceTasks("do_query");
 
     print_r(array(
         "randomIds" => array(
@@ -38,15 +59,20 @@ try {
             "district" => $randomDistrictId,
             "customer" => $randomCustomerId
         ),
-        "data" => $datas
+	"data" => $datas,
+	"debug" => getDebugInfo(),
+	"time" => microtime(1) - $beginTime
     ));
 
     $stmt = $db->query("commit");
+    traceTasks("do_commit");
 
     // close the connection
     $db = null;
 } catch (PDOException $e) {
-    header("HTTP/1.1 500 Internal Server Error");
-    error_log("error: ". $e->getMessage());
-    echo "Error: " . $e->getMessage();
+    traceTasks("error");
+    header("HTTP/1.1 501 Internal Server Error");
+    $errorMsg = "phpapp error: ". $e->getMessage(). " -- debug info : ". (getDebugInfo($e));
+    error_log($errorMsg);
+    echo $errorMsg;
 }
