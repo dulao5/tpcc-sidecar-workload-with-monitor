0f0feb54c1857118951f4804964331243c165ab8
Du Zhigang (ト シゴウ) <689799+dulao5@users.noreply.github.com>
2025-09-05 12:22:22 +0900

    Update base.scala

--- gatling/scenario/base.scala
+++ gatling/scenario/base.scala
@@ -7,7 +7,7 @@ import io.gatling.http.Predef._
 
 class TpccSidecarSimulation extends Simulation {
 
-  val maxClients = 300
+  val maxClients = 50
   val httpProtocol = http
     .baseUrl("http://nginx")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
